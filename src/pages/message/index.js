eeccb25b70472c0702907e4227b67fa5e62962d5
执念 <717503359@qq.com>
2018-12-12 10:46:33 +0800

    添加全局详细提示modal

--- src/pages/message/index.js
+++ src/pages/message/index.js
@@ -1,15 +1,24 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
 
 import './index.scss'
 
-
+@connect(({ message }) => ({
+    ...message
+}))
 export default class Message extends Component {
-    render() {
+
+    config = {
+        navigationBarTitleText: 'message' 
+    }
+
+    render () {
         return (
             <View>
-                <Text>Message</Text>
+                <Image mode="widthFix" src="http://static-r.msparis.com/uploads/8/a/8af1e11caa2c7dc29894777852d50eb3.png" />
             </View>
         )
     }
+
 }
